f109182587e09600ea1b70da0e80ada275c252e7
tryggvienator <tryggvienator@gmail.com>
2016-01-27 11:12:32 +0000

    Project added
    Whee.

---
+++ GithubTest/Assets/ChangeColorOnImage.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ChangeColorOnImage : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Image Image1;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void ChangeColorWhite () 
+	{
+		Image1.color = Color.white;
+	}
+	public void ChangeColorRed () 
+	{
+		Image1.color = Color.red;
+	}
+	public void ChangeColorBlue () 
+	{
+		Image1.color = Color.blue;
+	}
+	public void ChangeColorGreen () 
+	{
+		Image1.color = Color.green;
+	}
+
+}
